9776f2104338dc9f9a9042907aa160e9a7a8d5bd
Oliver Eriksson <oliver.stavsborg@gmail.com>
2017-10-26 13:38:15 +0200

    tweaked case_statement.rb

--- flow_Control/case_statement.rb
+++ flow_Control/case_statement.rb
@@ -1,10 +1,12 @@
 a = gets.chomp.to_i
 
-case a
+answer = case a
 when 5
-  p "a is 5"
+  "a is 5"
 when 6
-  p "a is 6"
+  "a is 6"
 else
-  p "a is neither 5 nor 6"
+  "a is neither 5 nor 6"
 end
+
+puts answer
