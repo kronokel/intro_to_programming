20c01e0d7459d5802fc34ad71d31cc5ae2f5a82c
Oliver Eriksson <oliver.stavsborg@gmail.com>
2017-10-26 18:08:16 +0200

    tweaked number.rb

--- flow_Control/number.rb
+++ flow_Control/number.rb
@@ -1,12 +1,44 @@
-puts "Please state a number"
-num = gets.chomp.to_i
-
-if num < 0
-  puts "You can't enter a negative number"
-elsif num <= 50
-  puts "The number is between 0 and 50"
-elsif num <= 100
-  puts "The number is between 51 and 100"
-else num > 100
-  puts "The number is above 100"
+def evaluate_num(num)
+  if num < 0
+    puts "You can't enter a negative number"
+  elsif num <= 50
+    puts "#{num} is between 0 and 50"
+  elsif num <= 100
+    puts "#{num} is between 51 and 100"
+  else
+    puts "#{num} is above 100"
+  end
 end
+
+def evaluate_num1(num)
+  case
+  when num < 0
+    puts "You can't enter a negative number"
+  when num <= 50
+    puts "#{num} is between 0 and 50"
+  when num <= 100
+    puts "#{num} is between 51 and 50"
+  else
+    puts "#{num} is above 100"
+  end
+end
+
+def evaluate_num2(num)
+  case num
+  when 0..50
+    puts "#{num} is between 0 and 50"
+  when 51..100
+    puts "#{num} is between 51 and 100"
+  else
+    if num < 0
+      puts "You can't enter a negative number"
+    else
+      puts "#{num} is above 100"
+    end
+  end
+end
+
+
+evaluate_num(24)
+evaluate_num1(51)
+evaluate_num2(-1)
